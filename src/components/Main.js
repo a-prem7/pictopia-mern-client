762e1fbca9891726afc5829e76482a7e1a19d0f8
amal-prem <aldamal.prem@gmail.com>
2023-05-20 20:26:58 -0400

    adds main.js

---
+++ src/components/Main.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Index from "../pages/Index";
+import Show from "../pages/Show";
+
+const Main = (props) => {
+  const [pic, setPic] = useState(null);
+  //   const url = "https://peopleapp-8vpg.onrender.com/people/";
+  const url = "http://localhost:4000/pic/";
+
+  const getPic = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setPic(data);
+  };
+
+  const createPic = async (spic) => {
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(spic),
+    });
+  };
+
+  const updatePeople = async (person, id) => {
+    // make put request to create people
+    await fetch(url + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(person),
+    });
+    // update list of people
+    getPeople();
+  };
+
+  const deletePeople = async (id) => {
+    // make delete request to create people
+    await fetch(url + id, {
+      method: "DELETE",
+    });
+    // update list of people
+    getPeople();
+  };
+
+  useEffect(() => getPeople(), []);
+
+  return (
+    <main>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={<Index people={people} createPeople={createPeople} />}
+        />
+        <Route
+          path="/people/:id"
+          element={
+            <Show
+              people={people}
+              updatePeople={updatePeople}
+              deletePeople={deletePeople}
+            />
+          }
+        />
+      </Routes>
+    </main>
+  );
+};
+
+export default Main;
