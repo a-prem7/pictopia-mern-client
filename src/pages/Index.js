f593838c70f6ef6208806d786a1be4e32161285c
amal-prem <aldamal.prem@gmail.com>
2023-05-20 20:35:47 -0400

    creates pages

---
+++ src/pages/Index.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Index(props) {
+  // state to hold formData
+  const [newForm, setNewForm] = useState({
+    title: "",
+    image: "",
+    description: "",
+  });
+
+  // handleChange function for form
+  const handleChange = (event) => {
+    setNewForm({ ...newForm, [event.target.name]: event.target.value });
+  };
+
+  // handle submit function for form
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.createPeople(newForm);
+    setNewForm({
+      title: "",
+      image: "",
+      description: "",
+    });
+  };
